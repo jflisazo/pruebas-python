50082d7be2f1f42d5e0aa1c86cb26bb273f6d67e
Juan Fernando Lisazo <fernando@Mac-mini-de-Juan.local>
2023-03-25 21:01:30 -0300

    ok agregar cubos

--- cuadrados.py
+++ cuadrados.py
@@ -1,2 +1,2 @@
 for i in range(10):
-    print(i,'\t',i*i, i*i*i, '\n')
+    print(i,'\t',i*i, '\t', i*i*i, '\n')
---
+++ lp/script1.py
@@ -0,0 +1,6 @@
+# A first Python script
+import sys
+print (sys.platform)    #Load a library module
+print (2 ** 100)
+x = 'spam!'
+print (x * 8)
